785b93d45b17bc65db2a92ebaa16ae3c4af47f73
prkat <7059682+prkat@users.noreply.github.com>
2025-07-04 14:23:34 +0200

    Update paths

--- paths.py
+++ paths.py
@@ -2,4 +2,4 @@ HOME_DIR = '/path/to/robustness_mer_bottleneck/'			# adapt this
 RUN_DIR = HOME_DIR + 'runs/'
 
 DATA_PATH_SOUNDTRACKS = '/path/to/Soundtracks/Set1/'				# adapt this
-ANNOTATIONS_PATH_MIDLEVEL = '/home/katharina/data/metadata_annotations'		# and this
+ANNOTATIONS_PATH_MIDLEVEL = '/path/to/metadata_annotations'		# and this
